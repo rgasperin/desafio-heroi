8a0a1aae1ac89be7da421e37dfb720c9f2e04243
Rafael <rafa123.gaspe@gmail.com>
2024-04-23 01:32:52 -0300

    Atualização no Desafio, adicionando Class

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Desafio do Héroi
 
 Codigo JS simples apenas com algumas variáveis, operadores, laços de repetição e estruturas de decisões, feitas para o desafio da Dio.me!
+
+## Desafio de Calcular Nível
+
+Agora pegamos a ideia do primeiro desafio e utilizamos as functions para deixar o codigo menor e mais dinamico.
+
+## Desafio Escrevendo as Classes de Um Jogo
+
+Agora pegamos a ideia do primeiro desafio e utilizamos as classes.
---
+++ desafio-heroi-class.js
@@ -0,0 +1,30 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque = "";
+
+    switch (this.tipo) {
+      case "mago":
+        ataque = "magia";
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+      default:
+        ataque = "um ataque indefinido";
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
